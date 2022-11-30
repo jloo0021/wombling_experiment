1c2c92526e788e5838f4bb917959e6435a11475c
Joshua Loong <jloo0021@student.monash.edu>
2022-11-30 16:07:44 +1100

    Added buffer creation for line geojson data to allow drawing of extrusions for dummy data

--- js/boundaries.js
+++ js/boundaries.js
@@ -1,4 +1,6 @@
-import geoJsonData from "../boundaries_SA1_2016.geojson" assert { type: "json" }; // app must be open on live server for imports/fetching to work
+// import geoJsonData from "../boundaries_SA1_2016.geojson" assert { type: "json" }; // app must be open on live server for imports/fetching to work
+// import geoJsonData from "../vic.geojson" assert { type: "json" };
+// import geoJsonData from "../dummy.geojson" assert { type: "json" }; // dummy data based on liveability geojson, with some coordinates manually converted using epsg.io
 
 // Could also use fetch instead of import
 // fetch("./boundaries_SA1_2016.geojson")
@@ -8,36 +10,45 @@ import geoJsonData from "../boundaries_SA1_2016.geojson" assert { type: "json" }
 //   .then((data) => console.log(data));
 
 /**
+ * Processes the source data so that it can be drawn using mapbox API
+ */
+function processData(sourceData) {
+  // sourceData.features[0].geometry.type = "Polygon";
+  // console.log(sourceData);
+  return turf.buffer(sourceData, 0.01, { units: "meters" }); // turns lines into polygons that can be extruded
+}
+
+/**
  * Draws a boundary between two areas on the map
  * @param {*} map
  */
-export function drawBoundary(map) {
+export function drawBoundary(map, sourceData) {
   // map is the mapbox map object that we're working on
-  // area1 and area2 are 2 different SAs (this will likely change depending on what the existing code looks like)
 
   console.log("drawBoundary called");
 
   // source defines the data
   let source = {
     type: "geojson",
-    data: {
-      type: "Feature",
-      properties: {
-        color: "gray",
-        height: 1000,
-        base_height: 0,
-      },
-      geometry: {
-        type: "Polygon",
-        coordinates: [
-          [
-            // dummy data, would have to get this from the areas somehow
-            [145.23, -37.816],
-            [145.177, -37.819],
-          ],
-        ],
-      },
-    },
+    // data: {
+    //   type: "Feature",
+    //   properties: {
+    //     color: "gray",
+    //     height: 1000,
+    //     base_height: 0,
+    //   },
+    //   geometry: {
+    //     type: "Polygon",
+    //     coordinates: [
+    //       [
+    //         // dummy data, would have to get this from the areas somehow
+    //         [145.23, -37.816],
+    //         [145.177, -37.819],
+    //       ],
+    //     ],
+    //   },
+    // },
+    data: processData(sourceData),
   };
 
   map.addSource("boundarySource", source);
@@ -48,9 +59,13 @@ export function drawBoundary(map) {
     type: "fill-extrusion",
     source: source,
     paint: {
-      "fill-extrusion-color": ["get", "color"],
-      "fill-extrusion-height": ["get", "height"],
-      "fill-extrusion-base": ["get", "base_height"],
+      // "fill-extrusion-color": ["get", "color"],
+      // "fill-extrusion-height": ["get", "height"],
+      // "fill-extrusion-base": ["get", "base_height"],
+      // "fill-extrusion-opacity": 0.5,
+      "fill-extrusion-color": "gray",
+      "fill-extrusion-height": 100,
+      // "fill-extrusion-height": ["get", "womble_scaled"], // need to somehow "get" this property and then manipulate it to create a scaled height0
       "fill-extrusion-opacity": 0.5,
     },
   };
--- js/map.js
+++ js/map.js
@@ -1,4 +1,5 @@
 import { drawBoundary } from "./boundaries.js";
+import geoJsonData from "../dummy.geojson" assert { type: "json" }; // dummy data based on liveability geojson, with some coordinates manually converted using epsg.io
 
 //mapbox token (taken from existing project)
 const MAPBOX_TOKEN =
@@ -7,7 +8,8 @@ const MAPBOX_TOKEN =
 let map = new mapboxgl.Map({
   container: "map",
   // center: [144.9631, -37.9631], // long lat of melb
-  center: [145.2, -37.817], // long lat of east side melb
+  // center: [145.2, -37.817], // long lat of east side melb
+  center: [145.0521072, -37.757202],
   zoom: 14,
   style: "mapbox://styles/mapbox/light-v11",
   accessToken: MAPBOX_TOKEN,
@@ -20,5 +22,5 @@ map.addControl(new mapboxgl.NavigationControl());
 
 map.on("load", () => {
   // when map loads, do...
-  drawBoundary(map);
+  drawBoundary(map, geoJsonData);
 });
