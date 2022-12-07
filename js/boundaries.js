3ab6fcc69d2766d6e05df47574427ed3bfbe6411
Joshua Loong <jloo0021@student.monash.edu>
2022-12-07 20:01:38 +1100

    renamed function

--- js/boundaries.js
+++ js/boundaries.js
@@ -51,7 +51,7 @@ function processData(sourceData) {
     "+proj=lcc +lat_0=0 +lon_0=134 +lat_1=-18 +lat_2=-36 +x_0=0 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs"
   );
 
-  sourceData = reprojectGeoJSON(
+  sourceData = reprojectGeoJson(
     proj4("EPSG:7845"),
     proj4("EPSG:4326"),
     sourceData
