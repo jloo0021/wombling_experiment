ca7ba7a2074fba0cf545473bdc81fe097f808e78
Joshua Loong <jloo0021@student.monash.edu>
2023-01-10 16:16:21 +1100

    Fixed minor bug w/ collapsible menu

--- js/collapsible.js
+++ js/collapsible.js
@@ -1,8 +1,10 @@
 export function initCollapsibleBehaviour() {
   let collapsible = document.getElementById("menu-collapsible");
+  let menuContents = document.getElementById("menu-contents");
+  menuContents.style.display = "block"; // initialised to block, so that the if statement in the event listener works correctly
+
   collapsible.addEventListener("click", () => {
-    collapsible.classList.toggle("active");
-    let menuContents = document.getElementById("menu-contents");
+    collapsible.classList.toggle("active"); // required for the +/- icons to appear correctly
     if (menuContents.style.display === "block") {
       menuContents.style.display = "none";
     } else {
