ca59baab8d1f294f6d9ca35db9ed0bdc67fd6d04
jloo0021 <jloo0021@student.monash.edu>
2023-02-01 15:33:09 +1100

    removed unneeded indicators json

--- js/indicatorOptions.js
+++ js/indicatorOptions.js
@@ -1,53 +1,51 @@
 // import { csvArray, csvToArr} from './upload.js';
-import indicatorsData from "../liveability_indicators_sa1_2016.json" assert { type: "json" };
-
 
 function createIndicatorOptions(optionsArray, select_id) {
-    // if (csvArray.Property == null) {
+  // if (csvArray.Property == null) {
 
-    // console.log(optionsArray);
-    var x = document.getElementById(select_id);
+  // console.log(optionsArray);
+  var x = document.getElementById(select_id);
 
-    for (let i = 0; i < optionsArray.length; i++) {
-        var option = document.createElement("option");
-        option.text = optionsArray[i];
-        x.add(option, i);
-    }
+  for (let i = 0; i < optionsArray.length; i++) {
+    var option = document.createElement("option");
+    option.text = optionsArray[i];
+    x.add(option, i);
+  }
 
-    var optionsTest = document.getElementById(select_id).options;
-    for (var i = 0; i < optionsTest.length; i++) {
-        console.log(optionsTest[i].value);
-    }
-    // }
+  var optionsTest = document.getElementById(select_id).options;
+  for (var i = 0; i < optionsTest.length; i++) {
+    console.log(optionsTest[i].value);
+  }
+  // }
 }
 
 function removeIndicatorOptions() {
-    var optionsTest = document.getElementById("indicators-selection").options;
-    for (var i = 0; i < optionsTest.length; i++) {
-        console.log(optionsTest[i].value);
-    }
-    var options = document.querySelectorAll('#indicators-selection option');
-    options.forEach(o => o.remove());
-    var optionsTest = document.getElementById("indicators-selection").options;
-    for (var i = 0; i < optionsTest.length; i++) {
-        console.log(optionsTest[i].value);
-    }
+  var optionsTest = document.getElementById("indicators-selection").options;
+  for (var i = 0; i < optionsTest.length; i++) {
+    console.log(optionsTest[i].value);
+  }
+  var options = document.querySelectorAll("#indicators-selection option");
+  options.forEach((o) => o.remove());
+  var optionsTest = document.getElementById("indicators-selection").options;
+  for (var i = 0; i < optionsTest.length; i++) {
+    console.log(optionsTest[i].value);
+  }
 }
 
 // Return an array of the selected option values
 // select is an HTML select element
 function gyug(select_id) {
-    var options = document.getElementById(select_id).selectedOptions;
-    var values = Array.from(options).map(({ value }) => value);
-    console.log(values)
-    return values;
+  var options = document.getElementById(select_id).selectedOptions;
+  var values = Array.from(options).map(({ value }) => value);
+  console.log(values);
+  return values;
 }
 
 function getValues(select_id) {
-    var options = document.getElementById(select_id).options;
-    var values = Array.from(options).map(({ value }) => value);
-    console.log(values)
-    return values;
+  var options = document.getElementById(select_id).options;
+  var values = Array.from(options).map(({ value }) => value);
+  console.log(values);
+  return values;
 }
 
 // Exporting variables and functions
